0f494e521b38ec43d5ba6cf6874e5e7e65491c29
Marco Antonio Gonzalez Castelan <marcoantonigoncas@gmail.com>
2023-04-24 14:25:55 -0600

    Agregado ejercicio SPAM bytes

---
+++ 001-Spam.ps1
@@ -0,0 +1,7 @@
+﻿function spam{
+    param([long]$tamanio)
+    $ran = New-Object System.Random
+    for($i = 0; $i -lt $tamanio; $i++){
+        Write-Host -NoNewline $ran.Next(0,2) -BackgroundColor "Black" -ForegroundColor "Green"
+    }
+}
